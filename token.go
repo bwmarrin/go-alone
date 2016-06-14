a64dc2733c860da643e58f158d32db0331ef6d0c
Bruce Marriner <bruce@sqls.net>
2016-06-13 23:33:22 -0500

    More steps.

--- token.go
+++ token.go
@@ -3,7 +3,10 @@ package authtoken
 import (
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -34,4 +37,45 @@ func Generate(expire time.Time, data, secret []byte) (*Token, error) {
 	t.HMAC = h.Sum(nil)
 
 	return &t, nil
+}
+
+// Verify accepts a string Token and returns true if it is valid.
+// maybe make this VerifyString? ughmn.
+func Verify(token string, secret []byte) (bool, error) {
+
+	parts := strings.Split(token, ".")
+
+	// make sure it's the right size
+	if len(parts) < 3 {
+		return false, fmt.Errorf("token is missing parts")
+	}
+
+	// convert the string expire to an int
+	expire, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return false, err
+	}
+
+	// check if the token is expired.
+	if time.Now().UTC().Unix() > int64(expire) {
+		return false, fmt.Errorf("expired token")
+	}
+
+	// verify HMAC
+	h := hmac.New(sha256.New, secret)
+	_, err = h.Write([]byte(fmt.Sprintf("%d.%s", expire, parts[1])))
+	if err != nil {
+		return false, err
+	}
+
+	nh := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	if parts[2] != string(nh) {
+		return false, fmt.Errorf("invalid signature")
+	}
+
+	return true, nil
+}
+
+func (t Token) String() string {
+	return fmt.Sprintf("%d.%s.%s", t.Expire.UTC().Unix(), t.Data, base64.StdEncoding.EncodeToString(t.HMAC))
 }
--- token_test.go
+++ token_test.go
@@ -1,5 +1,6 @@
 package authtoken
 
+import "fmt"
 import "testing"
 import "time"
 
@@ -15,4 +16,54 @@ func BenchmarkGenerate(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		Generate(expire, data, secret)
 	}
+}
+
+func TestGenerate(t *testing.T) {
+
+	data := []byte(`this is some data`)
+	secret := []byte(`zerorocks`)
+	expire := time.Now()
+
+	_, err := Generate(expire, data, secret)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestVerify(t *testing.T) {
+
+	data := []byte(`this is some data`)
+	secret := []byte(`zerorocks`)
+	expire := time.Now().Add(1 * time.Hour)
+
+	token, err := Generate(expire, data, secret)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b, err := Verify(token.String(), secret)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !b {
+		t.Fatal("token does not validate")
+	}
+}
+
+func ExampleGenerate() {
+
+	fmt.Println("Got here")
+
+	data := []byte(`this is some data`)
+	secret := []byte(`zerorocks`)
+	expire := time.Now()
+
+	token, err := Generate(expire, data, secret)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(token)
+	// Output: wrong_answer_on_purpose
 }
