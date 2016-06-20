4d2b39b871e80735c71a9413384d8439308920f0
Bruce Marriner <bruce@sqls.net>
2016-06-20 14:16:29 -0500

    Changes!

--- togoalone_test.go
+++ togoalone_test.go
@@ -1,7 +1,7 @@
 package togoalone
 
 import (
-	"reflect"
+	"crypto/subtle"
 	"testing"
 )
 
@@ -9,29 +9,72 @@ func TestSigner(t *testing.T) {
 
 	secret := []byte(`B1nzyRateLimits`)
 	data := []byte(`1203981209381290.LutinRocks`)
-	want := []byte{49, 50, 48, 51, 57, 56, 49, 50, 48, 57, 51, 56, 49, 50, 57, 48, 46, 76, 117, 116, 105, 110, 82, 111, 99, 107, 115, 46, 133, 212, 63, 127, 205, 94, 15, 192, 120, 198, 224, 171, 6, 110, 147, 101, 226, 74, 125, 5, 79, 155, 239, 75, 154, 29, 143, 53, 197, 242, 146, 43}
+	//	want := []byte{49, 50, 48, 51, 57, 56, 49, 50, 48, 57, 51, 56, 49, 50, 57, 48, 46, 76, 117, 116, 105, 110, 82, 111, 99, 107, 115, 46, 133, 212, 63, 127, 205, 94, 15, 192, 120, 198, 224, 171, 6, 110, 147, 101, 226, 74, 125, 5, 79, 155, 239, 75, 154, 29, 143, 53, 197, 242, 146, 43}
 
 	signer := New(secret)
 	token := signer.Sign(data)
 	token2 := signer.Sign(data)
 
-	if !reflect.DeepEqual(want, token) {
-		t.Fatal("created token is not valid")
-	}
-
-	if !reflect.DeepEqual(want, token) {
-		t.Fatal("created token2 is not valid")
-	}
-
-	if !reflect.DeepEqual(token, token2) {
+	if subtle.ConstantTimeCompare(token, token2) != 1 {
 		t.Fatal("token and token2 do not match")
 	}
 
-	ok := signer.Verify(token)
+	ok := signer.Validate(token)
 	if !ok {
-		t.Fatal("created token did not verify")
+		t.Fatal("created token did not validate")
+	}
+}
+
+func TestEncodeUint64(t *testing.T) {
+	b1 := []byte{102, 95, 95, 95, 95, 95, 95, 95, 95, 95, 56}
+	b2 := []byte{95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 56}
+	var i1 uint64 = 9223372036854775807
+	var i2 uint64 = 0xFFFFFFFFFFFFFFFF
+
+	if subtle.ConstantTimeCompare(b1, EncodeUint64(i1)) != 1 {
+		t.Fatal("EncodeUint64 returned bad value for i1")
 	}
 
+	if subtle.ConstantTimeCompare(b2, EncodeUint64(i2)) != 1 {
+		t.Fatal("EncodeUint64 returned bad value for i2")
+	}
+}
+
+func TestDecodeUint64(t *testing.T) {
+	var i uint64 = 0x00000000FFFFFFFF
+	b := EncodeUint64(i)
+	if DecodeUint64(b) != i {
+		t.Fatal("DecodeUint64 returned bad value for b1")
+	}
+
+	var i2 uint64 = 0xFFFFFFFFFFFFFFFF
+	b2 := EncodeUint64(i2)
+	if DecodeUint64(b2) != i2 {
+		t.Fatal("DecodeUint64 returned bad value for b2")
+	}
+}
+
+func BenchmarkEncodeUint64(b *testing.B) {
+
+	var ui uint64 = 0xFFFFFFFFFFFFFFFF
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		EncodeUint64(ui)
+	}
+}
+
+func BenchmarkDecodeUint64(b *testing.B) {
+
+	var i uint64 = 0x00000000FFFFFFFF
+	ei := EncodeUint64(i)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		DecodeUint64(ei)
+	}
 }
 func BenchmarkSignerSignLittle(b *testing.B) {
 	secret := []byte(`B1nzyRateLimits`)
@@ -83,7 +126,7 @@ func BenchmarkSignerReuseSignBig(b *testing.B) {
 	}
 }
 
-func BenchmarkSignerVerifyLittle(b *testing.B) {
+func BenchmarkSignerValidateLittle(b *testing.B) {
 	secret := []byte(`B1nzyRateLimits`)
 	data := []byte(`1203981209381290.LutinRocks`)
 
@@ -95,11 +138,11 @@ func BenchmarkSignerVerifyLittle(b *testing.B) {
 
 	for n := 0; n < b.N; n++ {
 		signer := New(secret)
-		_ = signer.Verify(token)
+		_ = signer.Validate(token)
 	}
 }
 
-func BenchmarkSignerReuseVerifyLittle(b *testing.B) {
+func BenchmarkSignerReuseValidateLittle(b *testing.B) {
 	signer := New([]byte(`B1nzyRateLimits`))
 	data := []byte(`1203981209381290.LutinRocks`)
 	token := signer.Sign(data)
@@ -108,11 +151,11 @@ func BenchmarkSignerReuseVerifyLittle(b *testing.B) {
 	b.ResetTimer()
 
 	for n := 0; n < b.N; n++ {
-		_ = signer.Verify(token)
+		_ = signer.Validate(token)
 	}
 }
 
-func BenchmarkSignerVerifyBig(b *testing.B) {
+func BenchmarkSignerValidateBig(b *testing.B) {
 	secret := []byte(`B1nzyRateLimits`)
 	data := []byte(`1203981209381290.7h90g7h089234g75908347gh09384h7v0897fg08947f5097423058974h908fg702f9j75028fg5704239hg7053498dj7249038jd57j097g5v029dh79hc47f507v9082h7f509234j7dc02d750j24935h7f924`)
 	signer := New([]byte(`B1nzyRateLimits`))
@@ -123,11 +166,11 @@ func BenchmarkSignerVerifyBig(b *testing.B) {
 
 	for n := 0; n < b.N; n++ {
 		signer := New(secret)
-		_ = signer.Verify(token)
+		_ = signer.Validate(token)
 	}
 }
 
-func BenchmarkSignerReuseVerifyBig(b *testing.B) {
+func BenchmarkSignerReuseValidateBig(b *testing.B) {
 	signer := New([]byte(`B1nzyRateLimits`))
 	data := []byte(`1203981209381290.7h90g7h089234g75908347gh09384h7v0897fg08947f5097423058974h908fg702f9j75028fg5704239hg7053498dj7249038jd57j097g5v029dh79hc47f507v9082h7f509234j7dc02d750j24935h7f924`)
 	token := signer.Sign(data)
@@ -136,6 +179,6 @@ func BenchmarkSignerReuseVerifyBig(b *testing.B) {
 	b.ResetTimer()
 
 	for n := 0; n < b.N; n++ {
-		_ = signer.Verify(token)
+		_ = signer.Validate(token)
 	}
 }
