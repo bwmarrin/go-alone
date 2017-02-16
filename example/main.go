56997981f8b6aa0e4de946e6d42ba632c4321b7a
Bruce Marriner <bruce@sqls.net>
2017-02-16 15:13:34 -0600

    Added more examples.

--- example/main.go
+++ example/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/bwmarrin/go-alone"
+import (
+	"crypto/sha256"
+	"time"
+
+	"github.com/bwmarrin/go-alone"
+)
 
 func main() {
 
@@ -49,4 +54,47 @@ func main() {
 		// signature is valid, it is safe to use the data
 		println(string(data3))
 	}
+
+	// You can pass options to your new signer with the Options{} struct
+	// like this.
+	o := new(goalone.Options)
+
+	// You have the signer add timestamp to each token like this
+	o.Timestamp = true
+
+	// You can even set a custom timestamp epoch, if you want.
+	// Just give it a unix timestamp in seconds and go-alone will
+	// use it as an offset of all timestamps. This will allow you
+	// to better future proof your tokens or to just make them
+	// more obsecure.
+	o.Epoch = 1293840000
+
+	// You can use a different algorithm if you wish for better security
+	// or a less secure one that is maybe faster? Up to you.
+	o.Algorithm = sha256.New
+
+	// Now lets pass these options to our new signer then sign our data
+	s = goalone.New(secret, o)
+	token = s.Sign(data)
+
+	// Of course you can do this all as a one liner too, but it does start to get
+	// a bit too long :)
+	token = goalone.New(secret, &goalone.Options{Timestamp: true, Epoch: 1293840000, Algorithm: sha256.New}).Sign(data)
+
+	// You can parse out a token into a struct that separates the payload and
+	// timestamp for you.
+	ts := s.Parse(token)
+
+	// Now, we can print the payload with
+	println(string(ts.Payload))
+
+	// We can print the timestamp with
+	println(ts.Timestamp.String())
+
+	// We can even check how old our timestamp is.  You can do a lot of other
+	// things with the Timestamp too, since it's a standard time.Time value.
+	if time.Since(ts.Timestamp) > time.Hour {
+		// That token's timestamp is over an hour old!
+	}
+
 }
